edc03b793913d5eb022cae0b55c3edd4ff4a35aa
Josemari <Josemari@DESKTOP-3AVSG5V>
2023-10-17 00:18:42 +0200

    si

--- src/test/java/GertaerakKopiatuDABTest.java
+++ src/test/java/GertaerakKopiatuDABTest.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
